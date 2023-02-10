323e230c80c80be30b4db723a7678d24efc66819
Pojzo <pojzinko8@gmail.com>
2023-02-10 18:53:01 +0100

    Skeleton for argument checking

--- parser.php
+++ parser.php
@@ -1,190 +1,161 @@
 <?php
-    
-    define("VARIABLE", 0);
-    define("SYMBOL", 1);
-    define("TYPE", 2);
-    define("LBL", 3);
 
-    // dictionary mappings where key is the name of instruction and value is an array of arguments to that instruction
-    $instructions_dic = ["MOVE"    => [VARIABLE, SYMBOL],
-                     "CREATEFRAME" => [],
-                     "PUSHFRAME"   => [],
-                     "POPFRAME"    => [],
-                     "DEFVAR"      => [VARIABLE],
-                     "CALL"        => [LBL],
-                     "RETURN"      => [],
-                     "PUSHS"       => [SYMBOL],
-                     "POPS"        => [VARIABLE],
-                     "ADD"         => [VARIABLE, SYMBOL, SYMBOL],
-                     "SUB"         => [VARIABLE, SYMBOL, SYMBOL],
-                     "MUL"         => [VARIABLE, SYMBOL, SYMBOL],
-                     "IDIV"        => [VARIABLE, SYMBOL, SYMBOL],
-                     "LT"          => [VARIABLE, SYMBOL, SYMBOL],
-                     "GT"          => [VARIABLE, SYMBOL, SYMBOL],
-                     "EQ"          => [VARIABLE, SYMBOL, SYMBOL],
-                     "AND"         => [VARIABLE, SYMBOL, SYMBOL],
-                     "OR"          => [VARIABLE, SYMBOL, SYMBOL],
-                     "NOT"         => [VARIABLE, SYMBOL],
-                     "INT2CHAR"    => [VARIABLE, SYMBOL],
-                     "STRI2INT"    => [VARIABLE, SYMBOL, SYMBOL],
-                     "READ"        => [VARIABLE, TYPE],
-                     "WRITE"       => [SYMBOL],
-                     "CONCAT"      => [VARIABLE, SYMBOL, SYMBOL],
-                     "STRLEN"      => [VARIABLE, SYMBOL],
-                     "GETCHAR"     => [VARIABLE, SYMBOL, SYMBOL],
-                     "SETCHAR"     => [VARIABLE, SYMBOL, SYMBOL],
-                     "TYPE"        => [VARIABLE, SYMBOL],
-                     "LABEL"       => [LBL],
-                     "JUMP"        => [LBL],
-                     "JUMPIFEQ"    => [LBL, SYMBOL, SYMBOL],
-                     "JUMPIFNEQ"   => [LBL, SYMBOL, SYMBOL],
-                     "EXIT"        => [SYMBOL],
-                     "DPRINT"      => [SYMBOL],
-                     "BREAK"       => [],
+define("VARIABLE", 0);
+define("SYMBOL", 1);
+define("TYPE", 2);
+define("LBL", 3);
+
+// dictionary mappings where key is the name of instruction and value is an array of arguments to that instruction
+$instructions_dic = ["MOVE"    => [VARIABLE, SYMBOL],
+    "CREATEFRAME" => [],
+    "PUSHFRAME"   => [],
+    "POPFRAME"    => [],
+    "DEFVAR"      => [VARIABLE],
+    "CALL"        => [LBL],
+    "RETURN"      => [],
+    "PUSHS"       => [SYMBOL],
+    "POPS"        => [VARIABLE],
+    "ADD"         => [VARIABLE, SYMBOL, SYMBOL],
+    "SUB"         => [VARIABLE, SYMBOL, SYMBOL],
+    "MUL"         => [VARIABLE, SYMBOL, SYMBOL],
+    "IDIV"        => [VARIABLE, SYMBOL, SYMBOL],
+    "LT"          => [VARIABLE, SYMBOL, SYMBOL],
+    "GT"          => [VARIABLE, SYMBOL, SYMBOL],
+    "EQ"          => [VARIABLE, SYMBOL, SYMBOL],
+    "AND"         => [VARIABLE, SYMBOL, SYMBOL],
+    "OR"          => [VARIABLE, SYMBOL, SYMBOL],
+    "NOT"         => [VARIABLE, SYMBOL],
+    "INT2CHAR"    => [VARIABLE, SYMBOL],
+    "STRI2INT"    => [VARIABLE, SYMBOL, SYMBOL],
+    "READ"        => [VARIABLE, TYPE],
+    "WRITE"       => [SYMBOL],
+    "CONCAT"      => [VARIABLE, SYMBOL, SYMBOL],
+    "STRLEN"      => [VARIABLE, SYMBOL],
+    "GETCHAR"     => [VARIABLE, SYMBOL, SYMBOL],
+    "SETCHAR"     => [VARIABLE, SYMBOL, SYMBOL],
+    "TYPE"        => [VARIABLE, SYMBOL],
+    "LABEL"       => [LBL],
+    "JUMP"        => [LBL],
+    "JUMPIFEQ"    => [LBL, SYMBOL, SYMBOL],
+    "JUMPIFNEQ"   => [LBL, SYMBOL, SYMBOL],
+    "EXIT"        => [SYMBOL],
+    "DPRINT"      => [SYMBOL],
+    "BREAK"       => [],
 ];
 
-   
-    // --------------------start of the script -------------------
 
-    // InputHandler class loads instructions and returns them for further processing
-    class InputHandler {
-        private $arg_count;
-        private $argv;
+// --------------------start of the script -------------------
 
-        function __construct(int $argc, array $argv) {
-            $this->arg_count = $argc - 1; // the first argument is always the file name
-            $this->argv = $argv;
-        }
+require('input_handler.php');
 
-        // print help message for parser.php
-        private function print_help() {
-            echo "Usage: php your_program.php [--help] [instructions]" .
-                "\n\nOptions:\n  --help    Show this help message\n\n";
-        }
+// Analyzer class checks for lexical and syntactical errors in instructions
+class Analyzer {
+    private $lines;
+    function __construct(array $lines) {
+        $this->lines = $lines;
+    }
 
-        public function handle_args() {
-            if ($this->arg_count > 1) {
-                // only one or no arguments are valid
-                exit(10);
+    # function removes comments from the line
+    private function remove_comment(string $line): string {
+        $comment_split = explode("#", $line);
+        $new_line = trim(reset($comment_split));
+
+        return $new_line;
+    }
+
+    private function check_variable_syntax(string $arguments) {
+         
+    }
+
+    private function check_symbol_syntax(string $arguments) {
+        
+    }
+
+    private function check_type_syntax(string $arguments) {
+        
+    }
+    private function check_label_syntax(string $arguments) {
+        
+    }
+
+    private function check_syntax(array $instruction_arguments, array $expected_arguments) {
+        for ($i = 0; $i < count($expected_arguments); $i++) {
+            switch ($expected_arguments[$i]) {
+                case VARIABLE:
+                    $this->check_variable_syntax($instruction_arguments[$i]);
+                    break;
+
+                case SYMBOL:
+                    $this->check_symbol_syntax($instruction_arguments[$i]);
+                    break;
+
+                case TYPE:
+                    $this->check_type_syntax($instruction_arguments[$i]);
+                    break;
+
+                case LBL:
+                    $this->check_label_syntax($instruction_arguments[$i]);
+                    break;
             }
-
-            if ($this->arg_count == 1) {
-                // only --help argument is accepted
-                if ($argv[1] != "--help") {
-                    exit(10);
-                }
-                $this->print_help();
-                exit(0); // terminate the program with exit code 0 after printing help
-            }
-        }
-
-        private function handle_header(array $lines): array {
-            // check if header is present
-            if ($lines[0] != ".IPPcode23") {
-                exit(21);
-            }
-
-            // remove the header
-            $lines = array_slice($lines, 1);
-            return $lines;
-        }
-
-        public function load_instructions(): array {
-            $instructions = "";
-
-            // read all instructions from stdin
-            while ($line = fgets(STDIN)) {
-                $instructions .= $line;
-            }
-
-            // split the input program into lines
-            $lines = explode("\n", $instructions);
-
-            // check header
-            $lines = $this->handle_header($lines);
-
-            // clear any whitespace from front and back
-            $clear_lines = array_map('trim', $lines);
-
-            // remove lines that start with a comment
-            $clear_lines = array_filter($clear_lines, function($line) {
-                return substr($line, 0, 1) != '#';
-            });
-            
-            // return non-empty lines
-            return array_filter($clear_lines, function($line) {
-                return !empty($line);
-            });
         }
     }
 
-    // Analyzer class checks for lexical and syntactical errors in instructions
-    class Analyzer {
-        private $lines;
-        function __construct(array $lines) {
-            $this->lines = $lines;
+    // return true if 'instruction' has the correct syntax
+    private function instruction_ok(string $instruction): bool {
+        global $instructions_dic;
+        // split by space and trim of whitespace
+        $split_instruction = explode(" ", $instruction);
+        $split_instruction = array_map('trim', $split_instruction);
+
+        $opcode = $split_instruction[0];                 // name of the instruction
+        echo $opcode . "\n";
+
+        // remove the opcode and reset indeces
+        $instruction_arguments = array_values(array_slice($split_instruction, 1)); // arguments of the instruction
+
+        // instruction doesn't exist
+        if (!array_key_exists($opcode, $instructions_dic)) {
+            echo "This instruction doesn't exist\n"; 
+            exit(22);
         }
 
-        # function removes comments from the line
-        private function remove_comment(string $line): string {
-            $comment_split = explode("#", $line);
-            $new_line = trim(reset($comment_split));
+        $expected_arguments = $instructions_dic[$opcode];
 
-            return $new_line;
+        // check if they're of the same length
+        if (count($expected_arguments) != count($instruction_arguments)) {
+            echo "Incorrect number of arguments\n";
+            exit(23);
         }
 
-        // return true if 'instruction' has the correct syntax
-        private function instruction_ok(string $instruction): bool {
-            global $instructions_dic;
+        /*
+        echo "These are the valid arguments for $opcode:"; 
+        print_r($expected_arguments);
+        echo " and we got";
+        print_r($instruction_arguments);
+         */
 
-            // split by space and trim of whitespace
-            $split_instruction = explode(" ", $instruction);
-            $split_instruction = array_map('trim', $split_instruction);
+        $this->check_syntax($instruction_arguments, $expected_arguments);
+        
+        return true;
+    }
 
-            $opcode = $split_instruction[0];                 // name of the instruction
-            echo $opcode . "\n";
-            $instruction_arguments = array_slice($split_instruction, 1); // arguments of the instruction
 
-            // instruction doesn't exist
-            if (!array_key_exists($opcode, $instructions_dic)) {
-                echo "This instruction doesn't exist\n"; 
+    public function analyze() {
+        echo print_r($this->lines);
+        for ($i = 0; $i < count($this->lines); $i++) {
+            $this->lines[$i] = $this->remove_comment($this->lines[$i]);
+            if (!$this->instruction_ok($this->lines[$i])) {
                 exit(22);
             }
-
-            $valid_arguments = $instructions_dic[$opcode];
-
-            // check if they're of the same length
-            if (count($valid_arguments) != count($instruction_arguments)) {
-                echo "Incorrect number of arguments\n";
-                exit(23);
-            }
-
-            echo "These are the valid arguments for $opcode:"; 
-            print_r($valid_arguments);
-            echo " and we got";
-            print_r($instruction_arguments);
-
-            return true;
-        }
-
-
-        public function analyze() {
-            echo print_r($this->lines);
-            for ($i = 0; $i < count($this->lines); $i++) {
-                echo $i . "\n";
-                $this->lines[$i] = $this->remove_comment($this->lines[$i]);
-                if (!$this->instruction_ok($this->lines[$i])) {
-                    exit(22);
-                }
-            }
         }
     }
+}
 
-    $input_handler = new InputHandler($argc, $argv);
-    $input_handler->handle_args();
+$input_handler = new InputHandler($argc, $argv);
+$input_handler->handle_args();
 
-    $lines = $input_handler->load_instructions();
+$lines = $input_handler->load_instructions();
 
-    $analyzer = new Analyzer(array_values($lines));
-    $analyzer->analyze();
+$analyzer = new Analyzer($lines, $instruction_dic);
+$analyzer->analyze();
 ?>
