2cdd9eb4eb7fce9d67ee83c71995d8cc1610b077
Pojzo <pojzinko8@gmail.com>
2023-02-13 16:40:35 +0100

    Pridal som turbo mode, takze to trva 10 krat kratsie, ale lutujem(asi)

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import subprocess
 import difflib
 import argparse
 import sys
+import multiprocessing
+import pickle
 
 BLUE = "\033[34m"
 BLACK = "\033[0m"
@@ -14,11 +16,13 @@ YELLOW = "\033[33m"
 
 class DirTester:
     class Tester:
-        def __init__(self, num_tests_, verbose, show_errors):
+        def __init__(self, num_tests_, verbose, show_errors, generated_test, turbo_mode):
             self.num_tests = num_tests_
             self.cur_test = 1
             self.verbose = verbose
             self.show_errors = show_errors
+            self.generated_test = generated_test
+            self.turbo_mode = turbo_mode
 
         def run_test(self, src: str):
             files = {}
@@ -41,11 +45,27 @@ class DirTester:
                                                                                                 files["out"].splitlines()], lineterm='', n=0)
             diff_result = list(diff)
             
+            if self.generated_test:
+                if diff_result:
+                    if self.turbo_mode:
+                        return False
+
+                    print(f"{RED} TEST [{self.cur_test}/{self.num_tests}] {os.path.basename(src)} unsuccessful {BLACK}")
+
+                
+                if self.turbo_mode:
+                    return True
+
+                print(f"{GREEN} TEST [{self.cur_test}/{self.num_tests}] {os.path.basename(src)} successful {BLACK}")
+                print("------------------------------")
+                return True
+
             if diff_result:
                 print(f"{RED} TEST [{self.cur_test}/{self.num_tests}] {os.path.basename(src)} unsuccessful {BLACK}")
                 if self.verbose:
                     print(files["src"])
                     print()
+
                 if files["rc"] != str(result.returncode):
                     print(f"{RED} expected return code {files['rc']} got {result.returncode} {BLACK}")
 
@@ -58,18 +78,20 @@ class DirTester:
 
                 print("------------------------------")
                 return False
-            else:
-                if files["rc"] != str(result.returncode):
-                    if self.verbose:
-                        print(files["src"])
-                        print()
-                    print(f"{RED} TEST [{self.cur_test}/{self.num_tests}] {os.path.basename(src)} unsuccessful - wrong \
-return code: expected {files['rc']} got {result.returncode}{BLACK}")
-                    return False
 
-                print(f"{GREEN} TEST [{self.cur_test}/{self.num_tests}] {os.path.basename(src)} successful {BLACK}")
-                print("------------------------------")
-                return True
+            if files["rc"] != str(result.returncode):
+                if self.verbose:
+                    print(files["src"])
+                    print()
+
+                print(f"{RED} TEST [{self.cur_test}/{self.num_tests}] {os.path.basename(src)} unsuccessful - wrong \
+                        return code: expected {files['rc']} got {result.returncode}{BLACK}")
+                return False
+
+            print(f"{GREEN} TEST [{self.cur_test}/{self.num_tests}] {os.path.basename(src)} successful {BLACK}")
+            print("------------------------------")
+
+            return True
 
 
     def __init__(self, directory_: str):
@@ -77,34 +99,40 @@ return code: expected {files['rc']} got {result.returncode}{BLACK}")
         self.files = glob.glob(self.directory + "/*.src")
         self.files = list(map(lambda x: x.replace(".src", ""), self.files))
 
-    def test_dir(self, verbose=True, show_errors=True):
+    def test_dir(self, verbose=True, show_errors=True, generated_test=False, turbo_mode=False, queue=None):
         success = 0
         print("{} Running tests for {} {}".format(BLUE, self.directory, BLACK))
-        tester = self.Tester(len(self.files), verbose, show_errors)
+        tester = self.Tester(len(self.files), verbose, show_errors, generated_test, turbo_mode)
         for file in self.files:
             success += tester.run_test(file)
             tester.cur_test += 1
 
-        print(f"{YELLOW} Finished [{success}/{len(self.files)}] successful {BLACK}")
+        print(f"{YELLOW} Finished {self.directory.split('/')[-1]} - [{success}/{len(self.files)}] successful {BLACK}")
         print()
+        if queue is not None:
+            serialized = pickle.dumps([success, len(self.files), self.directory])
+            queue.put(serialized)
+            return
+
         return success, len(self.files)
 
 def print_help():
     print("""usage: test.py [-h] [--run_generated RUN_GENERATED] [--show_errors SHOW_ERRORS]\n\
             [--verbose VERBOSE] [--test_dir TEST_DIR]\n
         optional arguments:\n\
-        ---------------------
+                ---------------------
             -h, --help\n\tshow this help message and exit\n\n\
-            --run_generated RUN_GENERATED\n\tSet to 1 to run generated code, set to 0 to not run\n\n\
-            --show_errors SHOW_ERRORS\n\tSet to 1 to show errors, set to 0 to not show\n\n\
-            --verbose VERBOSE\n\tSet to 1 to be verbose, set to 0 to not be verbose\n\n\
-            --test_dir TEST_DIR\n\tPath to test dir
+                    --run_generated RUN_GENERATED\n\tSet to 1 to run generated code, set to 0 to not run\n\n\
+                    --show_errors SHOW_ERRORS\n\tSet to 1 to show errors, set to 0 to not show\n\n\
+                    --verbose VERBOSE\n\tSet to 1 to be verbose, set to 0 to not be verbose\n\n\
+                    --test_dir TEST_DIR\n\tPath to test dir
 
   """)
     exit()
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('--run_generated', type=int, default=0, help='Set to True to run generated code')
+parser.add_argument('--turbo_mode', type=int, default=0, help='Use multiprocessing to speed up testing of generated tests')
 parser.add_argument('--show_errors', type=int, default=0, help='Set to True to show errors')
 parser.add_argument('--verbose', type=int, default=1, help='Set to True to be verbose')
 parser.add_argument('--test_dir', type=str, default="./IPP23_parser_tests/", help='Path to the tests')
@@ -119,18 +147,49 @@ except:
 if args.help:
     print_help()
 
+if not args.run_generated and args.turbo_mode:
+    print("Turbo mode is only available for generated tests")
+
 TEST_DIR = args.test_dir
+
+turbo_mode = args.turbo_mode
 
 generated_success, generated_total = 0, 0
 generated_summary = {}
 
 if args.run_generated:
-    for generated_folder in glob.glob(os.path.join(TEST_DIR + "GENERATED/*")):
-        dir_tester = DirTester(generated_folder)
-        result = dir_tester.test_dir(verbose=False, show_errors=args.show_errors)
-        generated_success += result[0]
-        generated_total += result[1]
-        generated_summary[generated_folder.split('/')[-1]] = [result[0], result[1]]
+    if args.turbo_mode:
+        queue = multiprocessing.Queue()
+        procs = []
+        generated_folders = glob.glob(os.path.join(TEST_DIR + "GENERATED/*"))
+        for generated_folder in generated_folders:
+            dir_tester = DirTester(generated_folder)
+            proc = multiprocessing.Process(target=dir_tester.test_dir, kwargs={'verbose': False, 'show_errors': args.show_errors,
+                                                                               'generated_test':True, 'queue': queue,
+                                                                               'turbo_mode': True})
+            procs.append(proc)
+            proc.start()
+
+        for proc in procs:
+            proc.join()
+
+        results = [queue.get() for _ in range(len(generated_folders))]
+        deserialized = [pickle.loads(result) for result in results]
+
+        for result1, result2, directory in deserialized:
+            generated_success += result1
+            generated_total += result2
+            directory = directory
+            generated_summary[directory.split('/')[-1]] = [result1, result2]
+    else:
+        for generated_folder in glob.glob(os.path.join(TEST_DIR + "GENERATED/*")):
+            dir_tester = DirTester(generated_folder)
+            result = dir_tester.test_dir(verbose=False, show_errors=args.show_errors, generated_test=True,)
+
+            generated_success += result[0]
+            generated_total += result[1]
+            generated_summary[generated_folder.split('/')[-1]] = [result[0], result[1]]
+
 
 success, total = 0, 0
 summary = {}
@@ -139,7 +198,7 @@ for folder in glob.glob(TEST_DIR + "*")[1:]:
         continue
 
     dir_tester = DirTester(folder)
-    result = dir_tester.test_dir(verbose=args.verbose, show_errors=args.show_errors)
+    result = dir_tester.test_dir(verbose=args.verbose, show_errors=args.show_errors, turbo_mode=turbo_mode)
     summary[folder.split('/')[-1]] = [result[0], result[1]]
     success += result[0]
     total += result[1]
